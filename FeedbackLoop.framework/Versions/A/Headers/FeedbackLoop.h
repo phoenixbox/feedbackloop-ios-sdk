674de72dd917ed66f3800fb540288b9d25d55df4
Shane Rogers <rogerssh@tcd.ie>
2015-05-02 00:08:08 -0700

    Reduced framework to core components

--- FeedbackLoop.framework/Versions/A/Headers/FeedbackLoop.h
+++ FeedbackLoop.framework/Versions/A/Headers/FeedbackLoop.h
@@ -2,24 +2,22 @@
 //  FeedbackLoop.h
 //  FeedbackLoop
 //
-//  Created by Shane Rogers on 4/26/15.
-//  Copyright (c) 2015 FBL. All rights reserved.
+//  Created by Shane Rogers on 4/27/15.
+//  Copyright (c) 2015 REPL. All rights reserved.
 //
 
 #import <Foundation/Foundation.h>
-#import <UIKit/UIKit.h>
-
-#if __IPHONE_OS_VERSION_MIN_REQUIRED < __IPHONE_7_0
-#error The FeedbackLoop iOS SDK supports iOS 7.0 upwards.
-#endif
-
 
 @interface FeedbackLoop : NSObject
 
-+ (void)setSlackApiKey:(NSString *)apiKey forAppId:(NSString *)appId;
++ (void)configureWithSlackToken:(NSString *)slackToken;
+
++ (void)setApiKey:(NSString *)apiKey forAppId:(NSString *)appId;
+
++ (void)registerUserWithEmail:(NSString *)userEmail;
+
++ (void)initWithAppId:(NSString *)appId;
 
 + (void)presentChatChannel;
-
-+ (void)presentConversationList;
 
 @end
